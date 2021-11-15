7ef208f8317c50f1cfb3727a3ebd3bff6f52bc1f
pc <pc@DESKTOP-0D1M21A>
2021-11-15 12:28:57 +0900

    first commit

---
+++ bnpparibas/src/test/java/com/bnpparibas/demo/BnpparibasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnpparibas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnpparibasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
